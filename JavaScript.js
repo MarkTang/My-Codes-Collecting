6007a8a833e1917aaae7613dceb7aa104e444b06
Mark Tang Wao <marktangwao@foxmail.com>
2016-05-22 15:03:26 +0800

    JSON data convert to JSON arry data.
    All JSON data convert to JSON array data;
    Convert JSON data if JSON data exist JSON array, else default;

--- JavaScript.js
+++ JavaScript.js
@@ -20,3 +20,57 @@ function bar()
 }
 
 bar()
+
+
+//
+// All JSON data convert to JSON array data;
+serializeJSONArray: function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name] === undefined) {
+            o[this.name] = this.value || '';
+        }
+        if (!o[this.name].push) {
+            o[this.name] = [o[this.name]];
+        }
+        else {
+            o[this.name].push(this.value || '');
+        }
+    });
+    return o;
+}
+
+
+ // Convert JSON data if JSON data exist JSON array, else default;
+ serializeJSON: function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+}
+
+$.fn.serializeJSON = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
