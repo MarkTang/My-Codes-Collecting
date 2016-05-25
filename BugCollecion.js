07b3f6f6d061d89f23eb975119f485178481be04
Mark Tang Wao <marktangwao@foxmail.com>
2016-05-26 01:26:25 +0800

    Upt

--- BugCollecion.js
+++ BugCollecion.js
@@ -1,7 +1,8 @@
 //Some Bugs
 
 
-B:"The model backing the context has changed since the database was created.".
+B:The model backing the 'ApplicationDbContext' context has changed since the database was created. 
+  Consider using Code First Migrations to update the database (http://go.microsoft.com/fwlink/?LinkId=238269).
 
 R:Database.SetInitializer<ApplicationDbContext>(null);
 
