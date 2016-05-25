71072bf72a33fc7663841e88d5c8f313d56bedf0
Mark Tang Wao <marktangwao@foxmail.com>
2016-05-26 01:11:13 +0800

    Create files~

---
+++ BugCollecion.js
@@ -0,0 +1,15 @@
+//Some Bugs
+
+
+B:Fixed error "The model backing the context has changed since the database was created.".
+  Update the optional information of patients in patient profile.
+
+R:Database.SetInitializer<ApplicationDbContext>(null);
+
+
+
+
+
+
+
+
