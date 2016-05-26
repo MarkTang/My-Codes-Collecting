fe3c4f365a93715834826b92629f4bb070c768f6
Mark Tang Wao <marktangwao@foxmail.com>
2016-05-27 00:39:35 +0800

    Test

--- JavaScript.js
+++ JavaScript.js
@@ -83,6 +83,62 @@ $.fn.serializeJSON = function () {
 
 
 
+//
+git branch --set-upstream marktangwao origin/marktangwao
+git branch --set-upstream <local_repo> origin/<remote_repo>
+
+
+
+
+(/^1[3|4|5|7|8]\d{9}$/).test("17033335555")
+
+
+
+
+<div class="col-md-2 text-right">
+<label>肿瘤类型:</label>
+</div>
+<div class="col-md-10">
+@{
+	foreach (var type in Enum.GetValues(typeof(CancerType)))
+	{
+		var stringValue = ((CancerType)type).GetStringValue();
+		<label class="col-md-3 pull-left">
+		<input type="checkbox" name="CancerType" value="@type" />
+		@stringValue
+		</label>
+	}
+}
+<br/>
+</div>
+
+
+Edit:
+<div class="form-group">
+<div class="col-md-2 text-right">
+<label>肿瘤类型:</label>
+</div>
+<div class="col-md-10">
+@{
+	List<CancerRecord> cancerType = Model.CancerTypeList;
+	foreach (var type in Enum.GetValues(typeof(CancerType)))
+	{
+		var stringValue = ((CancerType)type).GetStringValue();
+		bool existed = cancerType.FirstOrDefault(r => r.CancerType == (CancerType)type) != null;
+		<label class="col-md-3 pull-left">
+		<input type="radio" name="cancerType" value="@type" @(Html.Raw(existed ? "checked=\"true\"" : "") ) />
+		@stringValue
+		</label>
+	}
+}
+<br />
+</div>
+</div>
+
+
+
+"UserName", "PhoneNumber", "BirthDay", "Gender", "Age", "IdNumber", "CancerType","DiagnoseTime", "RelapseOrProgress", "Comment"
+
 
 
 
