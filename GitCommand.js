0ab0c10958a842519c9db8a7767d5ecff1081017
Mark Tang <marktangwao@foxmail.com>
2016-05-29 03:34:00 +0800

    Add new GitCommand file.

---
+++ GitCommand.js
@@ -0,0 +1,37 @@
+
+//Git Commands
+
+//Rename branch
+git branch --set-upstream marktangwao origin/marktangwao
+git branch --set-upstream <local_repo> origin/<remote_repo>
+
+
+//Create a file
+mkdir <folder name>
+touch <files name>
+
+//Rename a file name
+git mv <old name> <new name>
+
+
+$ ssh-keygen -t rsa -C "*@*.com"
+
+
+//05292016
+git clone https://*.visualstudio.com/ASP.NET%20MVC/_git/*
+
+git remote add origin https://*.visualstudio.com/ASP.NET%20MVC/_git/*
+
+git push -u origin --all
+
+
+//Generation SSH
+ssh-keygen -t rsa -C "*@*.com"
+
+
+//Git Commands
+
+
+
+
+
